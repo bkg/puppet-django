cf284dd39f88082924dc8a970d935cdaa8361bc7
Falk Schuetzenmeister <schuetzenmeister@berkeley.edu>
2013-08-01 10:57:04 -0700

    Update spatialdb.pp

--- manifests/spatialdb.pp
+++ manifests/spatialdb.pp
@@ -1,7 +1,7 @@
 # Enable PostGIS for the database.
 define django::spatialdb ($dbname, $pguser='postgres') {
   include django::postgis
-  $psql = "sudo -u ${pguser} psql -d ${dbname} -c"
+  $psql = "/bin/sh sudo -u ${pguser} psql -d ${dbname} -c"
   exec { "${psql} 'CREATE EXTENSION postgis;'":
     unless => "${psql} 'select postgis_version();'",
     require => Postgresql::Db[$dbname],
