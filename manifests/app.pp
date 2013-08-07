7e0b5403be8d52ad7b41e122bd25aed756ba2b07
Brian Galey <bkgaley@gmail.com>
2013-08-06 17:52:28 -0700

    Add virtualenv support to debian gunicorn config

--- manifests/app.pp
+++ manifests/app.pp
@@ -1,12 +1,14 @@
 define django::app (
   $vhostname,
+  $ensure = present,
   $vhostroot = '/var/www',
   $staticdir = 'static/public',
   $mediadir = 'media',
   $dbuser = undef,
   $dbpass = '',
+  $owner = 'root',
+  $group = 'root',
   $django = false,
-  $gunicorn_workers = $::processorcount * 2 + 1
 ) {
   $dbname = $name
   if $dbuser {
@@ -16,52 +18,57 @@ define django::app (
   }
   $vhostdocroot = "${vhostroot}/${vhostname}"
   $venvdir = "${vhostdocroot}/env"
-  file { $vhostroot:
-    ensure => directory,
-    owner => 'root',
-    group => 'root',
-  } ->
-  file { $vhostdocroot:
-    ensure => directory,
+  python::virtualenv { $venvdir:
+    owner => $owner,
+    group => $group,
   } ->
   file { "$vhostdocroot/$name":
     ensure => directory,
+    owner => $owner,
+    group => $group,
   }
 
   nginx::resource::upstream {"${name}_app":
-    ensure  => present,
+    ensure  => $ensure,
     members => [
       "unix:/var/run/gunicorn/${name}.sock",
     ],
   }
   nginx::resource::vhost {$vhostname:
-    ensure => present,
+    ensure => $ensure,
     proxy  => "http://${name}_app",
   }
   nginx::resource::location {$vhostname:
-    ensure   => present,
+    ensure   => $ensure,
     location_alias => "${vhostdocroot}/${name}/${name}/${staticdir}/",
     location => '/static/',
     vhost    => $vhostname,
   }
   nginx::resource::location {"${vhostname}-media":
-    ensure   => present,
+    ensure   => $ensure,
     location_alias => "${vhostdocroot}/${name}/${name}/${mediadir}/",
     location => '/media/',
     vhost    => $vhostname,
   }
 
-  python::venv::isolate { $venvdir: }
-  python::gunicorn::instance {$name:
-    venv => $venvdir,
-    src => "${vhostdocroot}/${name}",
-    django => true,
-    workers => $gunicorn_workers,
+  python::gunicorn { $name:
+    ensure => $ensure,
+    virtualenv  => $venvdir,
+    mode        => 'django',
+    dir         => "${vhostdocroot}/${name}",
+    bind        => "unix:/var/run/gunicorn/${name}.sock",
+    template    => 'django/gunicorn.erb',
+  }
+  python::pip { 'gunicorn':
+    virtualenv  => $venvdir,
+    owner => $owner,
+    require => Python::Virtualenv[$venvdir],
   }
   if $django {
-    python::pip::install { 'django':
-      venv => $venvdir,
-      require => Python::Venv::Isolate[$venvdir],
+    python::pip { 'django':
+      virtualenv  => $venvdir,
+      owner => $owner,
+      require => Python::Virtualenv[$venvdir],
     }
   }
 
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,19 +1,30 @@
 # Create a Django stack using Nginx, Gunicorn, and PostgreSQL with PostGIS.
 class django (
-  $nginx_workers = $::processorcount,
-  $owner = 'www-data',
+  $ensure = present,
   $geo = true,
+  $nginx_workers = $::processorcount,
 ) {
-  class {'nginx':
-    worker_processes => $nginx_workers,
+  $gunicorn_helper = '/usr/local/sbin/gunicorn-debian'
+  File {
+    owner => 'root',
+    group => 'root',
   }
-  class { 'python::venv':
-    owner => $owner,
-    group => $owner,
-  } ->
-  class { 'python::gunicorn':
-    owner => $owner,
-    group => $owner,
+  class { 'nginx': worker_processes => $nginx_workers }
+  class { 'python':
+    dev => true,
+    virtualenv => true,
+    gunicorn => true,
+    pip => true,
+  }
+  file { $gunicorn_helper:
+    ensure => $ensure,
+    mode => '0755',
+    source => 'puppet:///modules/django/gunicorn-debian',
+  }
+  file { '/etc/default/gunicorn':
+    content => "HELPER=$gunicorn_helper\n",
+    ensure => $ensure,
+    mode => '0644',
   }
   if $geo {
     include django::postgis
