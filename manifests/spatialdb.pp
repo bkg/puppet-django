de556e96d1f7449ad7be4f844e77d5b94192fcba
Brian Galey <bkgaley@gmail.com>
2013-08-22 18:04:59 -0700

    Pass user to exec instead of using sudo

--- manifests/spatialdb.pp
+++ manifests/spatialdb.pp
@@ -1,9 +1,10 @@
 # Enable PostGIS for the database.
 define django::spatialdb ($dbname, $pguser='postgres') {
   include django::postgis
-  $psql = "sudo -u ${pguser} psql -d ${dbname} -c"
+  $psql = "psql -d ${dbname} -c"
   exec { "${psql} 'CREATE EXTENSION postgis;'":
     unless => "${psql} 'select postgis_version();'",
+    user => $pguser,
     require => Postgresql::Db[$dbname],
   }
 }
