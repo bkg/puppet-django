3c21aeefc86b7337c0390dd5e3ca6aa44694636d
Brian Galey <bkgaley@gmail.com>
2013-09-11 12:00:16 -0700

    Allow overriding of postgis package name for default platforms

--- manifests/postgis.pp
+++ manifests/postgis.pp
@@ -1,26 +1,28 @@
 # Install PostGIS for PostgreSQL. Utilize Ubuntu GIS PPA if available.
-class django::postgis {
+class django::postgis ($pkgname = undef) {
   include postgresql::params
-  case $::operatingsystem {
-    'Ubuntu': {
-      $pkgname = "postgresql-$postgresql::params::version-postgis-2.0"
-      include apt
-      apt::ppa { 'ppa:ubuntugis/ubuntugis-unstable':
-        before => Package[$pkgname],
+  if !$pkgname {
+    case $::operatingsystem {
+      'Ubuntu': {
+        $postgis_name = "postgresql-$postgresql::params::version-postgis-2.0"
+        include apt
+        apt::ppa { 'ppa:ubuntugis/ubuntugis-unstable':
+          before => Package[$postgis_name],
+        }
+      }
+      'Debian': {
+        $postgis_name = "postgresql-$postgresql::params::version-postgis"
+      }
+      default: {
+        $postgis_name = $name
+        if $postgis_name == undef {
+          fail('Use PostGIS package name when not on Debian or Ubuntu')
+        }
       }
     }
-    'Debian': {
-      $pkgname = "postgresql-$postgresql::params::version-postgis"
-    }
-    default: {
-      $pkgname = $name
-      if $pkgname == undef {
-        fail('Use PostGIS package name when not on Debian or Ubuntu')
-      }
-    }
+  } else {
+    $postgis_name = $pkgname
   }
   include postgresql::devel
-  package { $pkgname:
-    ensure => present,
-  }
+  package {$postgis_name: ensure => present}
 }
