ae8537e4edae03eadb32f75e1f33bd5d9afe6483
Brian Galey <bkgaley@gmail.com>
2013-12-02 14:31:52 -0800

    Allow postgis package name to be provided

--- manifests/postgis.pp
+++ manifests/postgis.pp
@@ -1,28 +1,24 @@
 # Install PostGIS for PostgreSQL. Utilize Ubuntu GIS PPA if available.
-class django::postgis ($pkgname = undef) {
-  include postgresql::params
-  if !$pkgname {
-    case $::operatingsystem {
-      'Ubuntu': {
-        $postgis_name = "postgresql-$postgresql::params::version-postgis-2.0"
-        include apt
-        apt::ppa { 'ppa:ubuntugis/ubuntugis-unstable':
-          before => Package[$postgis_name],
-        }
-      }
-      'Debian': {
-        $postgis_name = "postgresql-$postgresql::params::version-postgis"
-      }
-      default: {
-        $postgis_name = $name
-        if $postgis_name == undef {
-          fail('Use PostGIS package name when not on Debian or Ubuntu')
-        }
+class django::postgis {
+  case $::operatingsystem {
+    'Ubuntu': {
+      $postgis_name = "postgresql-$postgresql::params::version-postgis-2.0"
+      include apt
+      apt::ppa { 'ppa:ubuntugis/ubuntugis-unstable':
+        before => Package[$postgis_name],
       }
     }
-  } else {
-    $postgis_name = $pkgname
+    'Debian': {
+      $postgis_name = "postgresql-$postgresql::params::version-postgis"
+    }
+    default: {
+      $postgis_name = $django::postgis
+    }
   }
+  if !$postgis_name or $postgis_name == true {
+    fail("Specify PostGIS package name when using $::operatingsystem.")
+  }
+  # We need postgres dev libs installed to compile psycopg2 in a virtualenv.
   include postgresql::devel
-  package {$postgis_name: ensure => present}
+  package { $postgis_name: ensure => present }
 }
